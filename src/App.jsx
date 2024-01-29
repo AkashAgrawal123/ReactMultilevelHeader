c415e1d171d44ebc153caa483a76f5d3c7b0ae62
akashagrawal9179 <akashagrawal5012@gmail.com>
2024-01-29 12:34:27 +0530

    #pushed changes without adding axios mock data setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import "./Styles/App.scss";
 import Navbar from "./Components/Navbar";
+import Homepage from "./Components/Homepage";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Homepage />
     </>
   );
 };
